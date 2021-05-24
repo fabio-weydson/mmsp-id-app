bc6e3ac773a86a61113f37b209249ca10d3c539b
Fábio Weydson <fabioweydson@gmail.com>
2021-05-24 02:45:13 -0300

    clean readme

--- src/App.js
+++ src/App.js
@@ -6,14 +6,8 @@ import {withAuthenticator} from 'aws-amplify-react';
 import Amplify, { Auth } from 'aws-amplify';
 import "./assets/css/material-dashboard-react.css?v=1.10.0";
 
-//import aws_exports from './aws-exports';
-Amplify.configure({
-  mandatorySignIn: true,
-  region: 'eu-central-1',
-  userPoolId:'eu-central-1_xvLnAMAmu',
-  identityPoolId: 'eu-central-1:e2c233fa-98f3-4647-bd0b-2a361abc549e',
-  userPoolWebClientId:'5u2t14f4r9udch0tvvl1uh0umn'
-});
+import aws_exports from './aws-exports';
+Amplify.configure(aws_exports);
 
 
 function App() {
